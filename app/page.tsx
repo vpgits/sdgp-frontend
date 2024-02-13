6a75b7c2ffcf324fb3395c6fa93b10053489207c
vpgits <vp.mb@protonmail.com>
2024-02-13 10:59:20 +0530

    Refactor profile page and landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 // pages/index.tsx
 
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
-import landingimage from '../public/landing.png';
-import Footer from '@/components/footer';
-import { Button } from '@/components/ui/button';
+import landingimage from "../public/landing.png";
+import Footer from "@/components/footer";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col md:flex-row items-center">
+      <main className="flex flex-col md:flex-row items-center pb-20">
         <div className="md:w-1/2  ml-20 mt-20">
-          <h1 className="text-5xl xs:text-center  md:text-5xl lg:text-7xl font-playfair leading-tight mb-3">
+          <h1 className="text-5xl xs:text-center  md:text-5xl lg:text-7xl font-inter leading-tight mb-3">
             The Best Way To Showcase Your Skills
           </h1>
           <p className="text-xl mt-10 md:text-2xl font-light md:mt-20">
@@ -24,7 +24,7 @@ export default function Home() {
                 Try For Free
               </Link>
             </Button>
-            <Button >
+            <Button>
               {/*className="border border-blue-500 text-blue-500 xl:ml-20"*/}
               <Link href="#buy" className="dark:text-black text-white">
                 Buy Premium
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,47 +1,67 @@
-import React from 'react';
-import Image from 'next/image';
-import logo from '../public/logo-max.png';
+import React from "react";
+import Image from "next/image";
+import logo from "../public/logo-max.png";
 
 const Footer = () => {
   return (
-    <footer className=" bg-indigo-950 text-white py-8 mt-20">
-      <div className="sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto">
-        
-      {/* <div className='mt-10'><hr /></div> */}
-      <div className='flex flex-auto justify-center flex-row'> 
+    <footer className=" bg-indigo-950 text-white p-5 flex flex-auto flex-col md:flex-row items-center justify-evenly">
+      {/* className="sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto flex flex-auto flex-row" */}
+
       <Image
-          src={logo}
-          alt="logo"
-          className="w-full h-auto mx-auto max-w-20 md:max-w-40 lg:max-w-48 xl:max-w-64"
-        />
+        src={logo}
+        alt="logo"
+        height={100}
+        width={100}
+        // className=" mx-auto max-w-20 md:max-w-40 lg:max-w-48 xl:max-w-64"
+      />
+      {/* <div className='mt-10'><hr /></div> */}
+
+      {/*container*/}
+
+      {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-4"> */}
+      <div className="space-y-4 ">
+        <h3 className="text-lg font-semibold mt-10 text-center">About</h3>
+        <p className="text-sm text-justify max-w-xs">
+          A group of five students in their second year at the University of
+          Informatics Institute of Technology developed Quzifyme, a generative
+          AI for a personalised and inventive approach to learning, based on a
+          solution that offers a personalised learning experience for anyone
+          seeking support from them. Quzifyme is a platform that assists users
+          with evaluating their own level of knowledge in various fields based
+          on the materials they submit as user materials within a specified
+          scope that meets their requirements.
+        </p>
+        <a href="#" className="text-sm hover:underline">
+          Learn more
+        </a>
       </div>
-      <div className="container mx-auto ">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="space-y-4 ">
-            <h3 className="text-lg font-semibold mt-10 text-center">About</h3>
-            <p className="text-sm text-justify">A group of five students in their second year at the University of Informatics Institute of Technology developed Quzifyme, a generative AI for a personalised and inventive approach to learning, based on a solution that offers a personalised learning experience for anyone seeking support from them. Quzifyme is a platform that assists users with evaluating their own level of knowledge in various fields based on the materials they submit as user materials within a specified scope that meets their requirements.</p>
-            <a href="#" className="text-sm hover:underline">Learn more</a>
-          </div>
-          <div className="space-y-4 mt-10  text-center ">
-            <h3 className="text-lg font-semibold md:w-1/2 mx-auto">Activity</h3>
-            <ul className="space-y-2 md:w-1/2 mx-auto">
-              <li><a href="#" className="text-sm hover:underline">Service 1</a></li>
-              <li><a href="#" className="text-sm hover:underline">Service 2</a></li>
-              <li><a href="#" className="text-sm hover:underline">Service 3</a></li>
-            </ul>
-          </div>
-          <div className="space-y-4 text-center">
-            <h3 className="text-lg font-semibold  mt-10 ">Contact</h3>
-            <p className="text-sm">123 Main Street, City, Country</p>
-            <p className="text-sm">Phone: +1 (123) 456-7890</p>
-            <p className="text-sm">Email: info@example.com</p>
-          </div>
-        </div>
-        <div className="mt-8">
-         
-        </div>
+      <div className="space-y-4 mt-10  text-center ">
+        <h3 className="text-lg font-semibold  mx-auto">Activity</h3>
+        <ul className="space-y-2  mx-auto">
+          <li>
+            <a href="#" className="text-sm hover:underline">
+              Service 1
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-sm hover:underline">
+              Service 2
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-sm hover:underline">
+              Service 3
+            </a>
+          </li>
+        </ul>
       </div>
+      <div className="space-y-4 text-center">
+        <h3 className="text-lg font-semibold  mt-10 ">Contact</h3>
+        <p className="text-sm">123 Main Street, City, Country</p>
+        <p className="text-sm">Phone: +1 (123) 456-7890</p>
+        <p className="text-sm">Email: info@example.com</p>
       </div>
+      {/* </div> */}
     </footer>
   );
 };
