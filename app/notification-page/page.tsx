10f3a5e9221aeb9084a8d55cd6e3c396c27ece9c
gimindugenuka <gimindu.20220282@iit.ac.lk>
2024-02-15 19:27:21 +0530

    Update dependencies and add notification page backend

--- app/notification-page/page.tsx
+++ app/notification-page/page.tsx
@@ -1,8 +1,41 @@
 import Notificationpage from "@/components/Notificationpage/Notificationpage";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+import { Tables,Database } from "@/types/supabase";
+export default async function Page() {
+  const cookieStore = cookies();
+  const supabase = createClient<Database>(cookieStore);
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/");
+  }
+
+  async function getNotification() {
+    const { data, error } = await supabase
+      .from("notification")
+      .select("*")
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      console.log(error);
+    }
+    return data!;
+  }
+
+  const notificationData:Tables<"notification">[] = await getNotification();
+  
+
+  revalidatePath("/notification-page");
+
+
+  
+  
   return (
-    <Notificationpage/>
+    <Notificationpage notificationData={notificationData} />
   )
 }
 
