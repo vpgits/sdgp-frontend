de6f9f006f7c940fa6adcbd146a0de49c98de823
gimindugenuka <gimindu.20220282@iit.ac.lk>
2024-03-12 19:38:34 +0530

    Update Notificationpage component and supabase types

--- components/Notificationpage/Notificationpage.tsx
+++ components/Notificationpage/Notificationpage.tsx
@@ -12,9 +12,9 @@ export default function Notificationpage(params: {
     
     <div className="flex flex-col">
       <main className="flex-1 overflow-y-auto p-6">
-        <div className="flex flex-auto justify-between my-2">
+        <div className="flex flex-auto justify-between my-8">
           <h1 className="text-2xl font-bold">Notifications</h1>
-          <Button variant="outline">Mark All as Read</Button>
+          {/* <Button variant="outline">Mark All as Read</Button> */}
         </div>
         
         {notifications.map((notification, index) => (
@@ -26,25 +26,12 @@ export default function Notificationpage(params: {
             <div className="grid gap-1">
               <p className="text-sm font-medium">{notification.title}</p>
               <p className="text-sm text-gray-500 dark:text-gray-400">
-                {notification["description "]}
+                {notification["description"]}
               </p>
             </div>
           </div>
         ))}
-        {/* <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-        <span className="flex h-2 w-2 translate-y-1.5 rounded-full bg-blue-500" />
-        <div className="grid gap-1">
-          <p className="text-sm font-medium">Your quiz is redy !</p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">1 min ago</p>
-        </div>
-      </div>
-      <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
-        <span className="flex h-2 w-2 translate-y-1.5 rounded-full bg-blue-500" />
-        <div className="grid gap-1">
-          <p className="text-sm font-medium">Your subscription is expiring soon!</p>
-          <p className="text-sm text-gray-500 dark:text-gray-400">2 hours ago</p>
-        </div>
-      </div> */}
+       
       </main>
       
     </div>
